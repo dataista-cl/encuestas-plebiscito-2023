25bb3316f1ea5d89e347293b8d85de9221125386
Fernando Becerra <becerrafernando@gmail.com>
2023-11-10 11:52:00 -0300

    add promedio buttons

--- js/main.js
+++ js/main.js
@@ -93,14 +93,28 @@ Promise.all([
     }
   });
 
+  const promButtons = d3.select("#promedio-buttons");
+
+  promButtons.selectAll("span")
+    .data(['1 encuesta', '3 encuestas', '5 encuestas'])
+    .join('span')
+      .html(d => d)
+      .on("click", (evt,d) => {
+        movingWindow = Number(d[0]);
+
+        console.log(movingWindow)
+
+        updatePlot();
+      })
+
   const x = 'fecha';
-  const yDots = 'valor';
+  let yDots = 'valor';
   const yLine = 'promedio';
   const circleRadius = 3,
     circleOpacity = 0.7;
-  const movingWindow = 3;
+  let movingWindow = 3;
 
-  const lines = [aprueba, desaprueba];
+  let lines = [aprueba, desaprueba];
   const colors = ["#2aad53", "#d934a1"]
 
   const xMin = d3.min(lines.map(d => d3.min(d, v => v[x])));
@@ -109,7 +123,6 @@ Promise.all([
   const yMax = 70;
 
   const movingAverage = (array, window, valueField, weightField) => {
-    let mvAvg = [];
     for (let idx = 0; idx < array.length; idx++) {
       const halfWindow = Math.floor(window / 2)
       const leftIdx = Math.max(0, idx - halfWindow);
@@ -120,9 +133,6 @@ Promise.all([
       array[idx].promedio = num / den;
     }
   }
-
-  movingAverage(aprueba, movingWindow, yDots, 'peso');
-  movingAverage(desaprueba, movingWindow, yDots, 'peso');
 
   const dates = aprueba.map(v => v.fecha);
 
@@ -165,125 +175,136 @@ Promise.all([
     .attr("fill", "#969696")
     .attr("stroke", "none");
 
-  svg.selectAll("circle .aprueba")
-      .data(aprueba)
+  const updatePlot = () => {
+    movingAverage(aprueba, movingWindow, yDots, 'peso');
+    movingAverage(desaprueba, movingWindow, yDots, 'peso');
+
+    lines = [aprueba, desaprueba]
+  
+    svg.selectAll("circle .aprueba")
+        .data(aprueba)
+        .join("circle")
+          .attr("class", "aprueba")
+          .attr("cx", d => xScale(d[x]))
+          .attr("cy", d => yScale(d[yDots]))
+          .attr("r", circleRadius)
+          .style("opacity", circleOpacity)
+          .style('fill', colors[0]);
+  
+    svg.selectAll("circle .desaprueba")
+      .data(desaprueba)
       .join("circle")
-        .attr("class", "aprueba")
+        .attr("class", "desaprueba")
         .attr("cx", d => xScale(d[x]))
         .attr("cy", d => yScale(d[yDots]))
         .attr("r", circleRadius)
         .style("opacity", circleOpacity)
-        .style('fill', colors[0]);
+        .style('fill', colors[1]);
 
-  svg.selectAll("circle .desaprueba")
-    .data(desaprueba)
-    .join("circle")
-      .attr("class", "desaprueba")
-      .attr("cx", d => xScale(d[x]))
-      .attr("cy", d => yScale(d[yDots]))
-      .attr("r", circleRadius)
-      .style("opacity", circleOpacity)
-      .style('fill', colors[1]);
-
-  const curves = svg.selectAll(".curve")
-      .data(lines)
-      .join("path")
-        .attr("class", "curve")
-        .attr("fill", "none")
-        .attr("stroke-width", 2.0)
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .style("mix-blend-mode", "multiply")
-        .style("opacity", 1.0)
-        .attr("stroke", (_, i) => colors[i])
-        .attr("d", d => line(d));
-
-  svg.selectAll(".label")
-    .data(lines.map(d => d[d.length - 1]))
-    .join("text")
-      .attr("class", "label")
-      .attr("x", d => xScale(d[x]) + 10)
-      .attr("y", d => yScale(d[yLine]) + 5)
-      .attr("fill", (_, i) => colors[i])
-      .attr("stroke", 'white')
-      .attr('stroke-width', 4)
-      .style('paint-order', 'stroke fill')
-      .text((d,i) => i === 0 ? `A favor ${d[yLine].toFixed(1)}%` : `En contra ${d[yLine].toFixed(1)}%`);
-
-  svg.on('mousemove', moved)
-      .on('mouseenter', entered)
-      .on('mouseleave', left)
-      .on('click', click);
-
-  function moved(event) {
-      const thisX = d3.pointer(event, this)[0];
-
-      if (thisX < width - margin.right) {
-        const xm = xScale.invert(thisX);
-        const i1 = d3.bisectLeft(dates, xm, 1);
-        const i0 = i1 - 1;
-        const i = xm - dates[i0] > dates[i1] - xm ? i1 : i0;
-
-        const af = aprueba[i].promedio;
-        const ec = desaprueba[i].promedio;
-
-        const textString = af > ec ? 'AF' : af < ec ? 'EC' : 'Empate';
-        const textColor = af > ec ? colors[0] : af < ec ? colors[1] : 'black';
-        const textDifference = af > ec ? (af - ec).toFixed(1) : af < ec ?(ec - af).toFixed(1) : 0;
-
-        rule.style('opacity', 1.0);
-
-        rule.select('line')
-            .attr("x1", xScale(dates[i]) - margin.left)
-            .attr("x2", xScale(dates[i]) - margin.left);
-
-        ruleText.attr("x", xScale(dates[i]) - margin.left + 5);
-          
-        ruleText.selectAll("tspan")
-          .data([d3.timeFormat("%d de %B")(dates[i]), `${textString} +${textDifference}%`])
-          .join("tspan")
-            .attr("x", xScale(dates[i]) - margin.left + 5)
-            .attr("dy", (_,i) => i === 0 ? 0 : 18)
-            .attr("class", (_,i) => i === 0 ? 'rule-date' : 'rule-difference')
-            .style('fill', (_,i) => i === 0 ? 'black' : textColor)
-            .text(d => d);
-
-        svg.selectAll(".label")
-            .data(lines.map(d => d[i]))
-            .join("text")
-              .attr("class", "label")
-              .attr("x", d => xScale(d[x]) + 10)
-              .attr("y", d => yScale(d[yLine]) + 5)
-              .attr("fill", (_, i) => colors[i])
-              .attr("stroke", 'white')
-              .attr('stroke-width', 4)
-              .text((d,i) => i === 0 ? `A favor ${d[yLine].toFixed(1)}%` : `En contra ${d[yLine].toFixed(1)}%`);
-      } else {
+    console.log(lines)
+  
+    const curves = svg.selectAll(".curve")
+        .data(lines)
+        .join("path")
+          .attr("class", "curve")
+          .attr("fill", "none")
+          .attr("stroke-width", 2.0)
+          .attr("stroke-linejoin", "round")
+          .attr("stroke-linecap", "round")
+          .style("mix-blend-mode", "multiply")
+          .style("opacity", 1.0)
+          .attr("stroke", (_, i) => colors[i])
+          .attr("d", d => line(d));
+  
+    svg.selectAll(".label")
+      .data(lines.map(d => d[d.length - 1]))
+      .join("text")
+        .attr("class", "label")
+        .attr("x", d => xScale(d[x]) + 10)
+        .attr("y", d => yScale(d[yLine]) + 5)
+        .attr("fill", (_, i) => colors[i])
+        .attr("stroke", 'white')
+        .attr('stroke-width', 4)
+        .style('paint-order', 'stroke fill')
+        .text((d,i) => i === 0 ? `A favor ${d[yLine].toFixed(1)}%` : `En contra ${d[yLine].toFixed(1)}%`);
+  
+    svg.on('mousemove', moved)
+        .on('mouseenter', entered)
+        .on('mouseleave', left)
+        .on('click', click);
+  
+    function moved(event) {
+        const thisX = d3.pointer(event, this)[0];
+  
+        if (thisX < width - margin.right) {
+          const xm = xScale.invert(thisX);
+          const i1 = d3.bisectLeft(dates, xm, 1);
+          const i0 = i1 - 1;
+          const i = xm - dates[i0] > dates[i1] - xm ? i1 : i0;
+  
+          const af = aprueba[i].promedio;
+          const ec = desaprueba[i].promedio;
+  
+          const textString = af > ec ? 'AF' : af < ec ? 'EC' : 'Empate';
+          const textColor = af > ec ? colors[0] : af < ec ? colors[1] : 'black';
+          const textDifference = af > ec ? (af - ec).toFixed(1) : af < ec ?(ec - af).toFixed(1) : 0;
+  
+          rule.style('opacity', 1.0);
+  
+          rule.select('line')
+              .attr("x1", xScale(dates[i]) - margin.left)
+              .attr("x2", xScale(dates[i]) - margin.left);
+  
+          ruleText.attr("x", xScale(dates[i]) - margin.left + 5);
+            
+          ruleText.selectAll("tspan")
+            .data([d3.timeFormat("%d de %B")(dates[i]), `${textString} +${textDifference}%`])
+            .join("tspan")
+              .attr("x", xScale(dates[i]) - margin.left + 5)
+              .attr("dy", (_,i) => i === 0 ? 0 : 18)
+              .attr("class", (_,i) => i === 0 ? 'rule-date' : 'rule-difference')
+              .style('fill', (_,i) => i === 0 ? 'black' : textColor)
+              .text(d => d);
+  
+          svg.selectAll(".label")
+              .data(lines.map(d => d[i]))
+              .join("text")
+                .attr("class", "label")
+                .attr("x", d => xScale(d[x]) + 10)
+                .attr("y", d => yScale(d[yLine]) + 5)
+                .attr("fill", (_, i) => colors[i])
+                .attr("stroke", 'white')
+                .attr('stroke-width', 4)
+                .text((d,i) => i === 0 ? `A favor ${d[yLine].toFixed(1)}%` : `En contra ${d[yLine].toFixed(1)}%`);
+        } else {
+          rule.style("opacity", 0.0);
+        }     
+    };
+  
+    function entered(event) {
+        
+    };
+  
+    function left(event) {
         rule.style("opacity", 0.0);
-      }     
-  };
-
-  function entered(event) {
-      
-  };
-
-  function left(event) {
-      rule.style("opacity", 0.0);
-
-      svg.selectAll(".label")
-        .data(lines.map(d => d[d.length - 1]))
-        .join("text")
-          .attr("class", "label")
-          .attr("x", d => xScale(d[x]) + 10)
-          .attr("y", d => yScale(d[yLine]) + 5)
-          .attr("fill", (_, i) => colors[i])
-          .attr("stroke", 'white')
-          .attr('stroke-width', 4)
-          .text((d,i) => i === 0 ? `A favor ${d[yLine].toFixed(1)}%` : `En contra ${d[yLine].toFixed(1)}%`);
-  };
-
-  function click(event) {
-      
-  };
+  
+        svg.selectAll(".label")
+          .data(lines.map(d => d[d.length - 1]))
+          .join("text")
+            .attr("class", "label")
+            .attr("x", d => xScale(d[x]) + 10)
+            .attr("y", d => yScale(d[yLine]) + 5)
+            .attr("fill", (_, i) => colors[i])
+            .attr("stroke", 'white')
+            .attr('stroke-width', 4)
+            .text((d,i) => i === 0 ? `A favor ${d[yLine].toFixed(1)}%` : `En contra ${d[yLine].toFixed(1)}%`);
+    };
+  
+    function click(event) {
+        
+    };
+  }
+  
+  updatePlot();
 
 })
